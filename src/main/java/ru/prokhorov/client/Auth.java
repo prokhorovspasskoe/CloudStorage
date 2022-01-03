5262767782aabe953e630b3f13b362256d1caacc
Prokhorov <prokhorovspasskoe@yandex.ru>
2022-01-03 19:03:38 +0300

    Add authorization and registration form

---
+++ src/main/java/ru/prokhorov/client/Auth.java
@@ -0,0 +1,12 @@
+package ru.prokhorov.client;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class Auth {
+    public TextField login;
+    public TextField password;
+
+    public void send(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/ru/prokhorov/client/Registration.java
@@ -0,0 +1,14 @@
+package ru.prokhorov.client;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class Registration {
+    public TextField login;
+    public TextField email;
+    public TextField password;
+    public TextField pass_conf;
+
+    public void send(ActionEvent actionEvent) {
+    }
+}

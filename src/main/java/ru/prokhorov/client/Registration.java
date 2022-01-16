7fec3ab12b1a26cf10f39a01ab8c1991755e005e
Prokhorov <prokhorovspasskoe@yandex.ru>
2022-01-16 08:56:24 +0300

    Send registration and show main window

--- src/main/java/ru/prokhorov/client/Registration.java
+++ src/main/java/ru/prokhorov/client/Registration.java
@@ -1,7 +1,15 @@
 package ru.prokhorov.client;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import ru.prokhorov.model.DatabaseConnection;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class Registration {
     public TextField login;
@@ -9,6 +17,27 @@ public class Registration {
     public TextField password;
     public TextField pass_conf;
 
-    public void send(ActionEvent actionEvent) {
+    public void send(ActionEvent actionEvent) throws SQLException, IOException {
+        String login = this.login.getText();
+        String password = this.password.getText();
+        String pass_conf = this.pass_conf.getText();
+        String email = this.email.getText();
+
+        if(!login.isEmpty() && !email.isEmpty() && !password.isEmpty() && !pass_conf.isEmpty()){
+            if(password.equals(pass_conf)){
+                DatabaseConnection databaseConnection = new DatabaseConnection();
+                databaseConnection.sendingRegistration(login, password, email);
+                if(databaseConnection.isReg()){
+                    Stage primary = (Stage) this.login.getScene().getWindow();
+                    primary.setScene(loadMainWindow());
+                    primary.show();
+                }
+            }
+        }
+    }
+
+    private Scene loadMainWindow() throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("cloud_client.fxml"));
+        return new Scene(parent);
     }
 }
